d663df5c324b63688e88733137512725e71e5f10
Ryan Chipwanya <ryxnxavier@gmail.com>
2023-01-20 21:56:56 +0300

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,40 +1,153 @@
-import React from 'react';
+import React, { useEffect, useState, useCallback, useMemo } from "react";
+import { Greet } from "./Greet";
 import {
   ChakraProvider,
   Box,
   Text,
-  Link,
   VStack,
-  Code,
+  HStack,
   Grid,
   theme,
-} from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
+  Button,
+  useToast,
+  Heading,
+  Code
+} from "@chakra-ui/react";
+import { ColorModeSwitcher } from "./ColorModeSwitcher";
+import * as web3 from "@solana/web3.js";
+import {
+  ConnectionProvider,
+  WalletProvider,
+  useConnection,
+  useWallet,
+} from "@solana/wallet-adapter-react";
+
+import {
+  PhantomWalletAdapter,
+  SolflareWalletAdapter,
+  SolletWalletAdapter,
+  SolletExtensionWalletAdapter,
+} from "@solana/wallet-adapter-wallets";
+
+import {
+  WalletModalProvider,
+  WalletMultiButton,
+} from "@solana/wallet-adapter-react-ui";
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+function useSolanaAccount() {
+  const [account, setAccount] = useState(null);
+  const [transactions, setTransactions] = useState(null);
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+
+  const init = useCallback(async () => {
+    if (publicKey) {
+      let acc = await connection.getAccountInfo(publicKey);
+      setAccount(acc);
+      let transactions = await connection.getConfirmedSignaturesForAddress2(
+        publicKey,
+        {
+          limit: 10,
+        }
+      );
+      setTransactions(transactions);
+    }
+  }, [publicKey, connection]);
+
+  useEffect(() => {
+    if (publicKey) {
+      setInterval(init, 1000);
+    }
+  }, [init, publicKey]);
+
+  return { account, transactions };
+}
+
+function Home() {
+  const { connection } = useConnection();
+  const { publicKey } = useWallet();
+  const { account, transactions } = useSolanaAccount();
+  const toast = useToast();
+  const [airdropProcessing, setAirdropProcessing] = useState(false);
+
+  const getAirdrop = useCallback(async () => {
+    setAirdropProcessing(true);
+    try {
+      var airdropSignature = await connection.requestAirdrop(
+        publicKey,
+        web3.LAMPORTS_PER_SOL
+      );
+      await connection.confirmTransaction(airdropSignature);
+    } catch (error) {
+      console.log(error);
+      toast({ title: "Airdrop failed", description: "unknown error" });
+    }
+    setAirdropProcessing(false);
+  }, [toast, publicKey, connection]);
+
+  return (
+    <Box textAlign="center" fontSize="xl">
+      <Grid minH="100vh" p={3}>
+        <ColorModeSwitcher justifySelf="flex-end" />
+        {publicKey && (
+          <VStack spacing={8}>
+            <Text>Wallet Public Key: {publicKey.toBase58()}</Text>
+            <Text>
+              Balance:{" "}
+              {account
+                ? account.lamports / web3.LAMPORTS_PER_SOL + " SOL"
+                : "Loading.."}
+            </Text>
+            <Button onClick={getAirdrop} isLoading={airdropProcessing}>
+              Get Airdrop of 1 SOL
+            </Button>
+            <Greet />
+            <Heading>Transactions</Heading>
+            {transactions && (
+              <VStack>
+                {transactions.map((v, i, arr) => (
+                  <HStack key={"transaction-" + i}>
+                    <Text>Signature: </Text>
+                    <Code>{v.signature}</Code>
+                  </HStack>
+                ))}
+              </VStack>
+            )}
+          </VStack>
+        )}
+        {!publicKey && <WalletMultiButton />}
+      </Grid>
+    </Box>
+  );
+}
 
 function App() {
+
+  //network in use 
+  const network = "devnet";
+  //Select the network (which is devnet) and connect to it 
+  const endpoint = web3.clusterApiUrl(network);
+  //array of wallets we want to use (and of course connected to devnet)
+  const wallets = useMemo(
+    () => [
+      new SolflareWalletAdapter(),
+      new PhantomWalletAdapter(),
+      new SolletWalletAdapter({ network }),
+      new SolletExtensionWalletAdapter({ network }),
+    ], [network]
+  );
+
+
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
-        <Grid minH="100vh" p={3}>
-          <ColorModeSwitcher justifySelf="flex-end" />
-          <VStack spacing={8}>
-            <Logo h="40vmin" pointerEvents="none" />
-            <Text>
-              Edit <Code fontSize="xl">src/App.js</Code> and save to reload.
-            </Text>
-            <Link
-              color="teal.500"
-              href="https://chakra-ui.com"
-              fontSize="2xl"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learn Chakra
-            </Link>
-          </VStack>
-        </Grid>
-      </Box>
+      <ConnectionProvider endpoint={endpoint}>
+        <WalletProvider wallets={wallets} autoConnect>
+          <WalletModalProvider>
+            <Home></Home>
+          </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
     </ChakraProvider>
   );
 }
---
+++ src/Greet.jsx
@@ -0,0 +1,150 @@
+import React, { useCallback, useEffect, useState } from 'react';
+import { VStack, HStack, Button, Text, Input } from '@chakra-ui/react';
+import { useConnection, useWallet } from '@solana/wallet-adapter-react';
+import * as borsh from 'borsh';
+import {
+    PublicKey,
+    Transaction,
+    SystemProgram,
+    TransactionInstruction,
+} from '@solana/web3.js';
+
+const programId = new PublicKey('FGbjtxeYmT5jUP7aNavo9k9mQ3rGQ815WdvwWndR7FF9');
+const GREETING_SEED = 'hello';
+
+class GreetingAccount {
+    counter = 0;
+    constructor(fields) {
+        if (fields) {
+            this.counter = fields.counter;
+        }
+    }
+}
+
+const GreetingSchema = new Map([
+    [GreetingAccount, { kind: 'struct', fields: [['counter', 'u32']] }],
+]);
+
+const GREETING_SIZE = borsh.serialize(
+    GreetingSchema,
+    new GreetingAccount()
+).length;
+
+function counterFromAccountInfo(accountInfo) {
+    const data = borsh.deserialize(
+        GreetingSchema,
+        GreetingAccount,
+        accountInfo.data
+    );
+    return data.counter;
+}
+
+export function Greet() {
+    const wallet = useWallet();
+    const { connection } = useConnection();
+    const [counter, setCounter] = useState(null);
+
+    const greet = useCallback(
+        async publicKey => {
+            const recipient = new PublicKey(publicKey);
+            const greetedPubkey = await PublicKey.createWithSeed(
+                recipient,
+                GREETING_SEED,
+                programId
+            );
+
+            const greetedAccount = await connection.getAccountInfo(greetedPubkey);
+            if (greetedAccount === null) {
+                const lamports = await connection.getMinimumBalanceForRentExemption(
+                    GREETING_SIZE
+                );
+
+                const transaction = new Transaction().add(
+                    SystemProgram.createAccountWithSeed({
+                        fromPubkey: recipient,
+                        basePubkey: recipient,
+                        seed: GREETING_SEED,
+                        newAccountPubkey: greetedPubkey,
+                        lamports,
+                        space: GREETING_SIZE,
+                        programId,
+                    })
+                );
+
+                const signature = await wallet.sendTransaction(transaction, connection);
+                await connection.confirmTransaction(signature, 'processed');
+            }
+
+            const instruction = new TransactionInstruction({
+                keys: [{ pubkey: greetedPubkey, isSigner: false, isWritable: true }],
+                programId,
+                data: Buffer.alloc(0),
+            });
+
+            const signature = await wallet.sendTransaction(
+                new Transaction().add(instruction),
+                connection
+            );
+
+            await connection.confirmTransaction(signature, 'processed');
+        },
+        [connection, wallet]
+    );
+
+    const greetYourself = useCallback(async () => {
+        await greet(wallet.publicKey.toBase58());
+    }, [greet, wallet.publicKey]);
+
+    useEffect(() => {
+        async function addListener() {
+            if (wallet.publicKey) {
+                const greetedPubkey = await PublicKey.createWithSeed(
+                    wallet.publicKey,
+                    GREETING_SEED,
+                    programId
+                );
+                const currentAccountInfo = await connection.getAccountInfo(
+                    greetedPubkey,
+                    'confirmed'
+                );
+                if (currentAccountInfo === null) {
+                    setCounter(0);
+                } else {
+                    setCounter(counterFromAccountInfo(currentAccountInfo));
+                }
+                connection.onAccountChange(
+                    greetedPubkey,
+                    (accountInfo, _) => {
+                        setCounter(counterFromAccountInfo(accountInfo));
+                    },
+                    'confirmed'
+                );
+            }
+        }
+        addListener();
+    }, [connection, wallet.publicKey]);
+
+    const [recipient, setRecipient] = useState('');
+    return (
+        <VStack width="full">
+            <HStack>
+                <Text>Total greetings: {counter === null ? 'Loading..' : counter}</Text>
+                <Button onClick={greetYourself}>Greet Yourself</Button>
+            </HStack>
+            <HStack>
+                <Text width="full">Greet Someone: </Text>
+                <Input
+                    value={recipient}
+                    onChange={e => setRecipient(e.target.value)}
+                ></Input>
+                <Button
+                    onClick={() => {
+                        greet(recipient);
+                    }}
+                >
+                    Greet
+                </Button>
+            </HStack>
+        </VStack>
+    );
+}
